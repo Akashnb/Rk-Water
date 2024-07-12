a3914165cf47538adf50ac3b885ab5053b57bfef
Akash Bambhaniya <akashbambhaniya@Akashs-MacBook-Air.local>
2024-07-12 20:06:55 +0530

    code refactoring

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,28 +1,23 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: ["./public/*.{html,js}"],
-    theme: {
-        extend: {
-            fontFamily: {
-                laila: ["Laila", "sans-serif"],
-                "be-vietnam": ["Be Vietnam", "sans-serif"],
-            },
-            colors: {
-                customBlue: "rgba(0, 77, 161, 1)",
-                "custom-blue-start": "rgba(0, 77, 161, 1)",
-                "custom-blue-end": "rgba(14, 62, 114, 1)",
-                "custom-bg": "rgba(231, 248, 255, 1)",
-            },
-            backgroundImage: {
-                "gradient-custom":
-                    "linear-gradient(to right, rgba(0, 77, 161, 1), rgba(14, 62, 114, 1))",
-                "btn-gradient":
-                    "linear-gradient(204.44deg, #004DA1 59.09%, #0E3E72 90.72%)",
-            },
-            height: {
-                70: "",
-            },
-        },
+  content: ["./public/*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        laila: ["Laila", "sans-serif"],
+        "be-vietnam": ["Be Vietnam", "sans-serif"],
+      },
+      colors: {
+        primary: "#004DA1",
+        secondary: "#21b6ff",
+        secondaryLight: "#e7f8ff",
+      },
+      backgroundImage: {
+        "gradient-custom": "linear-gradient(to right, #004DA1, #0E3E72)",
+        "btn-gradient":
+          "linear-gradient(204.44deg, #004DA1 59.09%, #0E3E72 90.72%)",
+      },
     },
-    plugins: [],
+  },
+  plugins: [],
 };
